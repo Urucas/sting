25cb0abbe6ee954cc8afb2852a11b4863e06cbfe
Bruno Alassia <vruno.alassia@gmail.com>
2014-07-08 20:01:59 -0300

    add socket.io library

---
+++ js/app.js
@@ -0,0 +1,58 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+app.get('*.js', function(req, res){
+	res.sendfile("public/js/"+req.path);
+});
+
+app.get('*.html', function(req,res){
+	res.sendfile("public/views/"+req.path);
+});
+
+app.get('*.css', function(req, res){
+	res.sendfile('public/css/'+req.path);
+});
+
+Array.prototype.shuffle = function(){
+	var o = this;
+	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+}
+
+var sockets = [];
+var listener = function(){
+	this.next = function(callback){
+		callback();
+	};
+	this.prev = function(callback){
+		callback();
+	}
+};
+var _listener = new listener();
+
+io.on('connection', function(socket){
+
+	socket.broadcast.emit("connected", _listener);
+
+	socket.on('disconnect', function(){
+	});
+
+	socket.on("next", function(){
+		console.log("next");
+	});
+
+	socket.on("prev", function(){
+		console.log("prev");
+	});
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
