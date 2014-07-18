35838541ce85fa52f839f16b366689adda5e90e1
Bruno Alassia <vruno.alassia@gmail.com>
2014-07-18 12:42:14 -0300

    create chat rooms to manage diferent presentations

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 var app = require('express')();
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('views/error.html');
 });
 
 app.get('*.js', function(req, res){
@@ -14,36 +15,52 @@ app.get('*.html', function(req,res){
 	res.sendfile("public/views/"+req.path);
 });
 
+app.get('.woff', function(){
+	res.sendfile("public/"+req.path);
+});
+
+app.get('.ttf', function(){
+	res.sendfile("public/"+req.path);
+});
+
+app.get('.svg', function(){
+	res.sendfile("public/"+req.path);
+});
+
 app.get('*.css', function(req, res){
 	res.sendfile('public/'+req.path);
 });
 
-var usersOnline = 0;
+var user, chat, usersOnline = 0;
+app.get('/:user/:presentation', function(req, res){
+	user = req.params;
+	chat = req.path;
+	res.sendfile("views/index.html");
+	
+	io.of(chat)	
+		.on('connection', function(socket){
+		usersOnline++;
+		console.log("Users online: "+usersOnline);
+		socket.on('disconnect', function(){
+			usersOnline--;
+		});
 
-io.on('connection', function(socket){
+		socket.on("left", function(){
+			socket.broadcast.emit("left");
+		});
 
-	usersOnline++;
-	console.log("Users online: "+usersOnline);
-	socket.on('disconnect', function(){
-		usersOnline--;
+		socket.on("right", function(){
+			socket.broadcast.emit("right");
+		});
+
+		socket.on("up", function(){
+			socket.broadcast.emit("up");
+		});
+
+		socket.on("down", function(){
+			socket.broadcast.emit("down");
+		});
 	});
-
-	socket.on("left", function(){
-		socket.broadcast.emit("left");
-	});
-
-	socket.on("right", function(){
-		socket.broadcast.emit("right");
-	});
-
-	socket.on("up", function(){
-		socket.broadcast.emit("up");
-	});
-
-	socket.on("down", function(){
-		socket.broadcast.emit("down");
-	});
-
 });
 
 http.listen(3000, function(){
