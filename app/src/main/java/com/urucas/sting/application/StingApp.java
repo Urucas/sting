118665c846f4ae26a23d81cda9bbb26bbcc9de34
Bruno Alassia <vruno.alassia@gmail.com>
2014-07-25 16:14:28 -0300

    add singleton patterns for api controller and persistance controller

--- app/src/main/java/com/urucas/sting/application/StingApp.java
+++ app/src/main/java/com/urucas/sting/application/StingApp.java
@@ -2,12 +2,17 @@ package com.urucas.sting.application;
 
 import android.app.Application;
 
+import com.urucas.sting.controller.ApiController;
+import com.urucas.sting.controller.PersistentController;
+
 /**
  * Created by Urucas on 7/23/14.
  */
 public class StingApp extends Application {
 
     private static StingApp _instance;
+    private static PersistentController _persistance;
+    private static ApiController _api;
 
     public StingApp() {
         super();
@@ -19,5 +24,19 @@ public class StingApp extends Application {
             _instance = new StingApp();
         }
         return _instance;
+    }
+
+    public static PersistentController getPersistance() {
+        if(_persistance == null){
+            _persistance = new PersistentController();
+        }
+        return _persistance;
+    }
+
+    public static ApiController getApiController(){
+        if(_api == null) {
+            _api = new ApiController();
+        }
+        return _api;
     }
 }
