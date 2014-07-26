a2b87aba0b03f12672aba8875218a2d1f6a87f12
Bruno Alassia <vruno.alassia@gmail.com>
2014-07-26 11:01:59 -0300

    add UserParser

---
+++ app/src/main/java/com/urucas/sting/parser/UserParser.java
@@ -0,0 +1,36 @@
+package com.urucas.sting.parser;
+
+import com.urucas.sting.model.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public abstract class UserParser {
+
+	public static User parse(JSONObject result) {
+		
+		try {
+
+            String token = result.getString("token");
+            JSONObject userObject = result.getJSONObject("user");
+
+			int id  = userObject.getInt("id");
+            String name = userObject.getString("name");
+            String email = userObject.getString("email");
+            String pass = userObject.getString("password");
+
+            User _user = new User();
+            _user.setId(id);
+            _user.setName(name);
+            _user.setEmail(email);
+            _user.setPass(pass);
+            _user.setToken(token);
+
+            return _user;
+
+		} catch (JSONException e) { e.printStackTrace(); }
+		
+		return null;
+	}
+	
+}
