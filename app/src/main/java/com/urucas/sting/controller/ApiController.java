df34bdb27041428ec5fd4d31193e367525f20550
Bruno Alassia <vruno.alassia@gmail.com>
2014-07-26 11:02:19 -0300

    add login API request method

--- app/src/main/java/com/urucas/sting/controller/ApiController.java
+++ app/src/main/java/com/urucas/sting/controller/ApiController.java
@@ -1,9 +1,20 @@
 package com.urucas.sting.controller;
 
+import android.content.Context;
 import android.provider.Settings.Secure;
 
 import com.urucas.sting.application.StingApp;
+import com.urucas.sting.callback.LoginCallback;
+import com.urucas.sting.model.CustomError;
+import com.urucas.sting.model.User;
+import com.urucas.sting.parser.ErrorParser;
+import com.urucas.sting.parser.UserParser;
+import com.urucas.sting.services.JSONRequestTask;
+import com.urucas.sting.services.JSONRequestTaskHandler;
 import com.urucas.sting.utils.Utils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ApiController {
 
@@ -27,5 +38,64 @@ public class ApiController {
         //checks internet connection
 		return Utils.isConnected(StingApp.singleton().getApplicationContext());
 	}
+
+    public void login(Context context, String email, String pass, final LoginCallback callback) {
+
+        if(!isConnected()) {
+            callback.onError(new CustomError("NO_CONNECTION"));
+            return;
+        }
+
+        String url = BASE_URL + "/login";
+        String imei = getUUID();
+
+        String nonce = Utils.generateNonce();
+        String repass = imei.substring(1,2);
+               repass+= Utils.md5(pass);
+               repass+= nonce;
+               repass = Utils.md5(repass);
+
+        new JSONRequestTask(new JSONRequestTaskHandler() {
+            @Override
+            public void onSuccess(JSONObject response) {
+                if(response.has("error")) {
+                    callback.onError(ErrorParser.parse(response));
+                    return;
+                }
+                User user = UserParser.parse(response);
+                if(user == null){
+                    callback.onError(new CustomError("ERROR_GETTING_USER"));
+                    return;
+                }
+                try {
+                    StingApp app = StingApp.singleton();
+                    app.getPersistance().createUser(
+                            app.getApplicationContext(),
+                            user
+                    );
+                    callback.onSuccess();
+                    return;
+
+                }catch(Exception e){
+                    callback.onError(new CustomError("ERROR_CREATING_SESSION"));
+                }
+            }
+
+            @Override
+            public void onSuccess(JSONArray result) {
+
+            }
+
+            @Override
+            public void onError(String message) {
+                callback.onError(new CustomError("INVALID_JSON_RESPONSE"));
+            }
+
+        }).addParam("e", email)
+                .addParam("i", imei)
+                .addParam("n", nonce)
+                .addParam("p", repass)
+                .execute(url);
+    }
 
 }
