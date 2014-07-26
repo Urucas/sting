3b6aab368c003296517756ebb2fd9baebb550952
Bruno Alassia <vruno.alassia@gmail.com>
2014-07-26 12:07:29 -0300

    fix getapicontroller call

--- app/src/main/java/com/urucas/sting/activities/ListActivity.java
+++ app/src/main/java/com/urucas/sting/activities/ListActivity.java
@@ -1,5 +1,6 @@
 package com.urucas.sting.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
@@ -13,8 +14,11 @@ import android.widget.TextView;
 import com.urucas.sting.R;
 import com.urucas.sting.adapters.SlideAdapter;
 import com.urucas.sting.application.StingApp;
+import com.urucas.sting.callback.SlidesNamespacesCallback;
+import com.urucas.sting.model.CustomError;
 import com.urucas.sting.model.SlideNamespace;
 import com.urucas.sting.model.User;
+import com.urucas.sting.utils.Utils;
 
 import java.util.ArrayList;
 
@@ -30,6 +34,8 @@ public class ListActivity extends ActionBarActivity {
     private ListView slideList;
     private SlideAdapter nspAdapter;
     private User user;
+    private ProgressDialog dialog;
+    private StingApp app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,14 +62,38 @@ public class ListActivity extends ActionBarActivity {
              }
         });
 
-        StingApp app = StingApp.singleton();
+        app = StingApp.singleton();
         try {
             user = app.getPersistance().getUser(ListActivity.this);
-            Log.i(TAG_NAME, user.getEmail());
+            getSlides(user);
 
         }catch(Exception e){
             app.logout(ListActivity.this);
         }
+    }
+
+    private void getSlides(User user){
+
+        dialog = ProgressDialog.show(ListActivity.this, "", getResources().getString(R.string.gettingslides), true);
+        dialog.setCancelable(false);
+        dialog.show();
+
+        app.getApiController().getSlides(user.getId(), new SlidesNamespacesCallback() {
+            @Override
+            public void onSuccess(ArrayList<SlideNamespace> namespaces) {
+
+            }
+
+            @Override
+            public void onError(CustomError error) {
+                onGetSlidesError(error);
+            }
+        });
+    }
+
+    private void onGetSlidesError(CustomError error) {
+        dialog.cancel();
+        Utils.Toast(ListActivity.this, error.getLocalizedMessage());
     }
 
     private void openSlideControl(SlideNamespace nsp) {
